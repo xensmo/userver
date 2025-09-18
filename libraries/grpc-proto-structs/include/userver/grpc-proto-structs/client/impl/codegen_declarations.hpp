52515d8b354522150257b9f141f21e319b939e42
kpavlov00 <kpavlov00@yandex-team.com>
2025-09-18 18:08:53 +0300

    feat grpc-proto-structs: added client/server codegen_declarations.hpp files commit_hash:6aec9f1be700621e43acf66b2d5a29ea582f1dd5

---
+++ libraries/grpc-proto-structs/include/userver/grpc-proto-structs/client/impl/codegen_declarations.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+// This header contains all static includes for code-generated
+// *_client.structs.usrv.pb.hpp files.
+//
+// Do not include this header in your code, use non-impl includes instead!
