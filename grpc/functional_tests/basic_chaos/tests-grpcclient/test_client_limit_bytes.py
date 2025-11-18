062f6172595639aecc29c47d65e31c86f186a257
antonyzhilin <antonyzhilin@userver.tech>
2025-11-18 13:16:55 +0300

    cc grpc: partially skip test_client_limit_bytes commit_hash:23f181aefb534b51ffed26041e58bff8954346e0

--- grpc/functional_tests/basic_chaos/tests-grpcclient/test_client_limit_bytes.py
+++ grpc/functional_tests/basic_chaos/tests-grpcclient/test_client_limit_bytes.py
@@ -5,6 +5,9 @@ import requests_client
 
 @pytest.mark.parametrize('case', requests_client.ALL_CASES)
 async def test_client_limit_bytes(grpc_ch, service_client, gate, case):
+    if case in ['say_hello_request_stream', 'say_hello_indept_streams']:
+        pytest.skip(reason='Fails with "RPC cancelled for servicer method" then timeout on testsuite side')
+
     for i in range(100, 250, 50):
         queue = await gate.to_client_limit_bytes(i)
         await requests_client.unavailable_request(service_client, gate, case)
