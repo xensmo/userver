930f4651eed19d4766da505b2fcd6ea3a82eb167
antoshkka <antoshkka@userver.tech>
2026-04-24 09:20:36 +0300

    feat universal: use C++20 chrono date
    For the userver/universal/include/userver/utils/datetime/cpp_20_calendar.hpp:
    * Preprocessed file size drops down from 3427913 to 2786765 bytes (-641148 bytes)
    * Includes count drops down from 824 to 662 (-162 includes)
    
    Migration hint:
    1) replace `date::make_time` with `std::chrono::hh_mm_ss`
    2) replace `date::` with `std::chrono::`
    3) replace `using namespace utils::datetime::date::literals` and `using namespace date::literals`  with `using namespace std::literals::chrono_literals;`
    4) If your standard library does not support `std::chrono::parse`, use `utils::datetime::UtcStringtime` from userver
    5) If your standard library does not support `std::chrono::format`, use `utils::datetime::UtcTimestring` from userver
    
    Tests: протестировано CI
    commit_hash:3f5f4f0707703b845ec8c40c2d0acbc63386aa6a

--- universal/include/userver/utils/datetime/cpp_20_calendar.hpp
+++ universal/include/userver/utils/datetime/cpp_20_calendar.hpp
@@ -1,16 +1,11 @@
 #pragma once
 
 /// @file userver/utils/datetime/cpp_20_calendar.hpp
-/// @brief This file brings date.h into utils::datetime::date namespace,
-/// which is std::chrono::operator/ (calendar) in c++20.
+/// @brief Helpers for C++20 calendar.
 /// @ingroup userver_universal
-
-// TODO : replace with C++20 std::chrono:: when time comes
 
 #include <chrono>
 #include <ratio>
-
-#include <date/date.h>
 
 #include <userver/utils/assert.hpp>
 
@@ -18,13 +13,13 @@ USERVER_NAMESPACE_BEGIN
 
 namespace utils::datetime {
 
-namespace date = ::date;
-
-using Days = date::days;
-using DaysTimepoint = date::sys_days;
+// TODO: remove the following aliases
+namespace date = std::chrono;
+using Days = std::chrono::days;
+using DaysTimepoint = std::chrono::sys_days;
 
 /// @brief Calculates the number of days between January 1, 00:00 of two years accounting for leap years.
-constexpr Days DaysBetweenYears(int from, int to) {
+constexpr std::chrono::days DaysBetweenYears(int from, int to) {
     return std::chrono::duration_cast<Days>(
         DaysTimepoint{date::year_month_day(date::year(to), date::month(1), date::day(1))} -
         DaysTimepoint{date::year_month_day(date::year(from), date::month(1), date::day(1))}
@@ -32,7 +27,7 @@ constexpr Days DaysBetweenYears(int from, int to) {
 }
 
 /// @brief Get the number of days in the given month of a given year.
-constexpr date::day DaysInMonth(int month, int year) {
+constexpr std::chrono::day DaysInMonth(int month, int year) {
     UINVARIANT(month >= 1 && month <= 12, "Month must be between 1 and 12");
     return date::year_month_day_last(date::year(year), date::month_day_last(date::month(month))).day();
 }
